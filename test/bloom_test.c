c506c0df70bce1d7f0af7d238dd408f784b7b711
Wenhao Luo <nemoluo@live.com>
2026-09-18 15:40:27 +0800

    update `bloom_test`

--- test/bloom_test.c
+++ test/bloom_test.c
@@ -4,7 +4,12 @@
 int main() {
     bloom_filter_t *filter = bloom_init();
     bloom_add(filter, "测试词汇");
+
     bool is_ok = bloom_exist(filter, "测试词汇");
     assert(is_ok);
+
+    is_ok = bloom_exist(filter, "词汇");
+    assert(!is_ok);
+
     bloom_free(filter);
 }
